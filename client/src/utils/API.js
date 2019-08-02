3527cefc9e4a11036516c90c16d8f93c8e251edc
Roman <romansenin15@gmail.com>
2019-08-02 13:28:13 -0700

    biolerplate modifications

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // The getBooks method retrieves books from the server
-// It accepts a "query" or term to search the recipe api for
+// It accepts a "query" or term to search the google books api for
 export default {
   getBooks: function(query) {
     return axios.get("/api/books", { params: { q: query } });
