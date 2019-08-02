4f66df443bb281db3996eaac256892e2a3b78fbc
Roman <romansenin15@gmail.com>
2019-08-02 13:38:03 -0700

    initialized App state

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,22 @@ import React, { Component } from "react";
 import API from "./utils/API";
 
 class App extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+      booksSearch: ""
+    };
+  }
+
   handleFormSubmit = event => {
     // When the form is submitted, prevent its default behavior, get recipes update the recipes state
     event.preventDefault();
-    API.getBooks(this.state.recipeSearch)
-      .then(res => this.setState({ recipes: res.data }))
+    API.getBooks(this.state.booksSearch)
+      .then(res => this.setState({ books: res.data }))
       .catch(err => console.log(err));
   };
+  
   render() {
     return (
       <>
