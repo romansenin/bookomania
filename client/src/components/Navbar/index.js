463aa7de59b8c75fbb79f54f623df37a6d58f19c
Roman <romansenin15@gmail.com>
2019-08-02 17:56:28 -0700

    included Navbar Links

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return(<h1>Navbar</h1>)
-}
+  return (
+    <nav className="navbar navbar-default">
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <Link className="navbar-brand" to="/">
+            Google Books
+          </Link>
+        </div>
+        <ul className="nav navbar-nav">
+          <li className={window.location.pathname === "/" ||
+            window.location.pathname === "/search"
+            ? "active"
+            : ""}>
+            <Link to="/">Search</Link>
+          </li>
+
+          <li className={window.location.pathname === "/saved"
+            ? "active"
+            : ""}>
+            <Link to="/">Search</Link>
+          </li>
+
+        </ul>
+      </div>
+    </nav >
+  );
+};
 
 export default Navbar;
