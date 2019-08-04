1e97f6bdd64197d24b0026f3cedb210cce27e41e
Roman <romansenin15@gmail.com>
2019-08-03 20:22:00 -0700

    wrote up Results content

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Results = () => {
-  return(<h1>Results</h1>);
+class Results extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: ""
+    }
+  }
+  render() {
+    return (
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Results</h5>
+          <div>{this.state.content === "" ? "Search for a book to see the results." : this.state.content}</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Results;
