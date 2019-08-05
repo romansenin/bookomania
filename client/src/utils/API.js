e0e192d08976fd760239276fa2ddb324e9eae609
Roman <romansenin15@gmail.com>
2019-08-05 14:32:58 -0700

    wrote saveBook API function

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-// The getBooks method retrieves books from the server
-// It accepts a "query" or term to search the google books api for
 export default {
   getBooks: function(query) {
     return axios.get("/api/books", { params: { q: query } });
+  },
+  saveBook: function(bookObj) {
+    return axios.post("/api/books", { data: { book: bookObj } });
   }
 };
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,33 @@
 const axios = require("axios");
 const router = require("express").Router();
+const db = require("../client/models");
 
 router.get("/books", (req, res) => {
   axios
-    .get("https://www.googleapis.com/books/v1/volumes?q=" + req.query.q + "&printType=books")
+    .get(
+      "https://www.googleapis.com/books/v1/volumes?q=" +
+        req.query.q +
+        "&printType=books"
+    )
     .then(results => {
       res.json(results.data);
     })
     .catch(err => {
       res.status(422).send(err);
+    });
+});
+
+router.post("/books", (req, res) => {
+  db.Book.create(req.body.book)
+    .then(function(dbBook) {
+      // View the added result in the console
+      console.log(dbBook);
+      res.sendStatus(200).end();
+    })
+    .catch(function(err) {
+      // If an error occurred, log it
+      console.log(err);
+      res.sendStatus(500).end();
     });
 });
 
